2744cd2b480e27c9ccd1355bdca4cd2933318d88
Gilliard <sgityriq@amazon.com>
2023-08-28 17:20:07 -0400

    Added 13 products into product.js as well as imported the images from index.js

--- audical/src/assets/index.js
+++ audical/src/assets/index.js
@@ -1,4 +1,29 @@
-// import appleLogo from "./appleLogo.png";
+import MakeAWay from "./MakeAWay.jpg";
+import MustardnMacSauce from "./MustardnMacSauce.jpg";
+import LookingBack from "./LookingBack.jpg";
+import TiltedTowers from "./TiltedTowers.jpg";
+import BLUFF from "./BLUFF.jpg";
+import RelishParts from "./RelishParts.jpg";
+import ProfessorX from "./ProfessorX.jpg";
+import DateNight from "./DateNight.jpg";
+import Atrocity from "./Atrocity.jpg";
+import AloneTime from "./AloneTime.jpg";
+import BentleyBeats from "./BentleyBeats.jpg";
+import thankthestars from "./thankthestars.jpg";
+import forestfires from "./forestfires.jpg";
 
 
-// export { appleLogo };
+export { MakeAWay };
+export { MustardnMacSauce };
+export { LookingBack };
+export { TiltedTowers };
+export { BLUFF };
+export { RelishParts };
+export { ProfessorX,};
+export { DateNight };
+export { Atrocity };
+export { AloneTime };
+export { BentleyBeats };
+export { thankthestars };
+export { forestfires };
+
